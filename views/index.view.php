c19e039d844e3fd2b7485649da7e75c0a6654e96
Guilherme <gzbordignon@gmail.com>
2021-02-17 01:01:34 -0300

    commit

--- views/index.view.php
+++ views/index.view.php
@@ -1,16 +1,16 @@
 <?php require 'partials/head.php' ?>
 	<div id="home-page">
 		<div class="container mx-auto w-10/12 my-10 flex flex-col items-center">
-				<h1 class="text-center uppercase font-bold text-3xl mb-10">Minhas Tarefas</h1>
+				<h1 class="text-center uppercase font-bold text-3xl mb-10">Tarefas</h1>
 				<div class="flex w-6/12 justify-between mb-10">
-					<a href="/search?q=pending" class="bg-yellow-500 py-3 w-3/12 text-center uppercase text-white font-bold">Pendentes</a>
-					<a href="/search?q=done" class="bg-green-500 py-3 w-3/12 text-center uppercase text-white font-bold">Feitas</a>
-					<a href="/search?q=all" class="bg-blue-500 py-3 w-3/12 text-center uppercase text-white font-bold">Todas</a>
+					<a href="/search?q=pending" class="bg-yellow-500 py-2 w-3/12 text-center uppercase text-white font-bold rounded hover:bg-yellow-600 ring-4 ring-yellow-300">Pendentes</a>
+					<a href="/search?q=done" class="bg-blue-500 py-2 w-3/12 text-center uppercase text-white font-bold rounded hover:bg-blue-600 ring-4 ring-blue-300">Feitas</a>
+					<a href="/search?q=all" class="bg-red-500 py-2 w-3/12 text-center uppercase text-white font-bold rounded hover:bg-red-600 ring-4 ring-red-300">Todas</a>
 				</div>
-				<table class="border border-collpase w-4/12 mb-20">
-					<tbody class="border">
+				<table class="w-4/12 mb-20">
+					<tbody class="">
 						<?php foreach ($tasks as $task) : ?>
-							<tr class="border">
+							<tr class="border-b border-black">
 								<?php if ($task->completed) : ?>
 									<td class="p-3 pl-5"><i class="far fa-check-square"></i></td>
 									<td class="p-3">
@@ -39,10 +39,10 @@
 				</table>
 
 				<h2 class="mb-5 uppercase text-2xl font-bold">Adicionar Tarefa</h2>
-				<form action="tasks" method="POST" class="w-6/12">
+				<form action="tasks" method="POST" class="w-4/12">
 					<label for="description">Descrição</label> <br>
-					<input type="text" name="description" class="border p-2 mb-5 w-full"> <br>
-					<input type="submit" value="Adicionar tarefa" class="bg-blue-600 p-2 w-full uppercase text-white cursor-pointer font-bold">
+					<input type="text" name="description" class="border border-black p-2 mb-5 w-full rounded"> <br>
+					<input type="submit" value="Adicionar tarefa" class="bg-blue-600 p-2 w-full uppercase text-white cursor-pointer font-bold hover:bg-blue-700 rounded">
 				</form>
 		</div>
 	</div>
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -1,8 +1,8 @@
 <nav class="bg-green-400">
-	<div class="container mx-auto w-10/12 p-4">
+	<div class="container mx-auto w-10/12 p-4 flex justify-between items-center">
+		<a href="" class="text-white uppercase font-bold flex items-center"><i class="fas fa-tasks fa-2x"></i>   </a>
 		<ul class="flex">
 			<li><a href="/" class="mr-4 uppercase font-bold text-white">Home</a></li>
-			<li><a href="/about" class="uppercase font-bold text-white">About</a></li>
 		</ul>
 	</div>
 </nav>
