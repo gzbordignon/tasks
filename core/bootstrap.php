eab5ce178fd776a0c72e5507f8e8193c2974fe06
Guilherme <gzbordignon@gmail.com>
2021-02-16 21:25:49 -0300

    commit

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,15 +1,7 @@
 <?php
 
-// $app = [];
-
 App::bind('config', require 'config.php');
 
 App::bind('database', new QueryBuilder(
 	Connection::make(App::get('config')['database'])
 ));
-
-// $app['config'] = require 'config.php';
-
-// $app['database'] =  new QueryBuilder(
-// 	Connection::make($app['config']['database'])
-// );
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
- 
 require 'vendor/autoload.php';
-require 'core/bootstrap.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+ 
+ die($_ENV['APP_ENV']);
 
-Router::load('routes.php')->direct(Request::uri(), Request::method());
+
+// require 'core/bootstrap.php';
+
+
+
+
+
+// Router::load('routes.php')->direct(Request::uri(), Request::method());
