6df695c9376dff1459ce30cf083d5fed9bd139df
Guilherme <gzbordignon@gmail.com>
2021-02-16 22:20:08 -0300

    commit

--- core/database/Connection.php
+++ core/database/Connection.php
@@ -6,9 +6,16 @@ class Connection
 	public static function make($config)
 	{
 	    try {
-			return new PDO(
-				$config['driver'].':host='.$config['host'].';port='.$config['port'].';dbname='.$config['dbname'], $config['username'], $config['password'], $config['options']
-			);
+	    	if ($_ENV['APP_ENV'] === 'development') {
+				return new PDO(
+					$config['driver'].':host='.$config['host'].';port='.$config['port'].';dbname='.$config['dbname'], $config['username'], $config['password'], $config['options']
+				);
+	    	} else {
+				return new PDO(
+					$config['driver'].':host='.$config['host'].';port='.$config['port'].';dbname='.$config['dbname'].';user='.$config['username'].';password='.$config['password']
+				);
+	    	}
+
 		} catch (PDOException $e) {
 			// $e->getMessage();
 			die('Could not connect');
--- index.php
+++ index.php
@@ -6,7 +6,4 @@ $dotenv->load();
 
 require 'core/bootstrap.php';
 
-
-
-
 Router::load('routes.php')->direct(Request::uri(), Request::method());
